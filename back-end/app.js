3e122882591f1d51ceb121ae103417fc2be66c0e
GeorgTYQ <tanyouquan28@gmail.com>
2025-05-31 09:27:29 +0000

    fix ( Connect DB)

--- back-end/app.js
+++ back-end/app.js
@@ -9,6 +9,8 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 //解析请求体（可以直接解释json格式的请求体）
 import bodyParser from 'body-parser'; //
+import { config } from './config.js'; //配置文件
+import nodemon from 'nodemon'; //自动重启服务
 import {PrismaClient} from '@prisma/client';
 
 
@@ -22,10 +24,74 @@ const port = process.env.PORT || 3000
 console.log("running on port " + port);
 
 
-app.unsubscribe(cors({
+app.use(cors({
   origins:"*",
   methods:['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders:['Content-Type']
 }))
 
 app.use(bodyParser.json())
+
+//读取用户
+app.get('/users', async (req, res) => {
+  try {
+    //等待读取用户数据，当有数据时，赋值users
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to read users' });
+  }
+
+}
+)
+
+
+
+//读取单个用户
+app.get('/users/:id', async (req, res) => {
+  try {
+    //等待读取用户数据，当有数据时，赋值users
+    const users = await prisma.user.findUnique({
+      where:{id:req.params.id}
+    });
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to read users' });
+  }
+
+}
+)
+
+//写用户
+app.post('/users', async (req, res) => {
+  try{
+    const {name,email,html5,css3,javascript} = req.body; //获取请求体中的新用户数据
+    //进行判断是否数据齐全
+    if (!name || !email || !html5 || !css3 || !javascript) {
+      //用户方 error
+      return res.status(400).json({ error: 'All fields are required' });
+      
+    }
+    const users = await prisma.user.create({
+      name,
+      email,
+      html5,
+      css3,
+      javascript,
+    }); //读取用户数据
+    res.status(201).json(newUser); //返回新用户数据
+  }catch (error) {
+    //数据库方 error
+    res.status(500).json({ error: 'Failed to write users' });
+  }
+
+})
+
+
+//监听端口
+app.listen(
+  config.port, 
+  () => {
+    console.log(`Server is running on port ${config.port}`);
+  }
+); 
